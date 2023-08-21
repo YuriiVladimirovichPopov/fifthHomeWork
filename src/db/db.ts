7535f8c72140c20b60e4c84c8890e61590930874
YuriiVladimirovichPopov <papanchik87@yahoo.com>
2023-08-21 19:48:05 +0500

    fix 1

---
+++ src/db/db.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { BlogsMongoDbType, PostsMongoDbType, createPostDTOType } from '../types';
+import { MongoClient, WithId } from 'mongodb'
+
+
+const url = process.env.mongoUrl 
+
+console.log(url)
+if (!url) {
+  throw new Error('Url is not exsist')
+}
+
+export const client = new MongoClient(url)
+
+export const blogsCollection = client.db().collection<BlogsMongoDbType>('blogs')
+
+export const postsCollection = client.db().collection<PostsMongoDbType>('posts')
+
+export const  runDb = async () => {
+  try {
+    await client.connect();
+    console.log('connected successfully to database')
+  } catch (err) {
+    console.log('error connecting')
+    await client.close();
+  }
+}
+
+
+
+
---
+++ src/routers/helpers/pagination.ts
@@ -0,0 +1,49 @@
+export type PaginatedType = {
+    pageNumber: number,
+    pageSize: number,
+    sortDirection: 'asc' | 'desc',
+    sortBy: string,
+    skip: number,
+    searchNameTerm?: string
+}
+
+export const getPaginationFromQuery = (query: any): PaginatedType => {
+
+    const defaultValues: PaginatedType = {
+        searchNameTerm: '',
+        pageNumber: 1, 
+        pageSize: 10, 
+        sortDirection: 'desc',
+        sortBy: 'createdAt',
+        skip: 0
+    }
+        if(query.sortBy){
+            defaultValues.sortBy = query.sortBy
+        }
+    
+        if(query.sortDirection && query.sortDirection === 'asc') { 
+             defaultValues.sortDirection = query.sortDirection 
+        }
+    
+        if(query.pageNumber  && !isNaN(parseInt(query.pageNumber, 10)) && parseInt(query.pageNumber, 10) > 0) {
+             defaultValues.pageNumber = parseInt(query.pageNumber, 10)
+        } 
+    
+        if(query.pageSize  && !isNaN(parseInt(query.pageSize, 10)) && parseInt(query.pageSize, 10) > 0) {
+            defaultValues.pageSize = parseInt(query.pageSize, 10)
+       } 
+           
+        if(query.searchNameTerm) {
+            defaultValues.searchNameTerm = query.searchNameTerm
+        }
+    defaultValues.skip = (defaultValues.pageNumber - 1) * defaultValues.pageSize
+
+    return defaultValues
+
+
+
+
+}
+    
+    
+    
